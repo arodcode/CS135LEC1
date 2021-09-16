c82b23dd3c421c0000080e95db375fbe302cd5a5
arodcode <rodria93@unlv.nevada.edu>
2021-09-16 14:01:31 -0700

    CS135

---
+++ main.cpp
@@ -0,0 +1,27 @@
+/* Name: Alejandro Rodriquez, 2001889172, CS135-1001, Assignment 1
+Description: Printing string of inititials to the console.
+Input:None.
+Output:String of characters made into my initials.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+        cout << "    Alejandro Rodriquez                                                " << endl;  // name
+        cout << "    CS135 Assignment 1                                                 " << endl;  // assignment #
+        cout << "               AA                      AA            RRRRRRRRRRRR      " << endl;  // first line of initials
+        cout << "              AAAA                    AAAA           RR          RR    " << endl;
+        cout << "             AA  AA                  AA  AA          RR          RR    " << endl;
+        cout << "            AA    AA                AA    AA         RR          RR    " << endl;
+        cout << "           AA      AA              AA      AA        RR         RR     " << endl;
+        cout << "          AAAAAAAAAAAA            AAAAAAAAAAAA       RRRRRRRRRRRRR     " << endl;
+        cout << "         AA          AA          AA          AA      RR           RR   " << endl;
+        cout << "        AA            AA        AA            AA     RR            RR  " << endl;
+        cout << "       AA              AA      AA              AA    RR            RR  " << endl;
+        cout << "      AA                AA    AA                AA   RR            RR  " << endl;
+        cout << "     AA                  AA  AA                  AA  RR            RR  " << endl;  // last line of initials
+        return 0;
+}
